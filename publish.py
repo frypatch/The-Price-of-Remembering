f529f363d9191fe63800db2473f4ab8e40da7b7c
frypatch <georgemcarlson@gmail.com>
2025-05-21 20:21:32 -0500

    Simplify SItemap XML
    Simplify sitemap.xml

--- publish.py
+++ publish.py
@@ -495,11 +495,7 @@ def get_sitemap_XML(markdown_filenames):
     lastmod = datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%dT%H:%M:%S%z')
     lastmod = lastmod[:-2] + ':' + lastmod[-2:] 
     all_xhtml = """<?xml version="1.0" encoding="UTF-8"?>
-<urlset
-      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">"""
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">"""
     all_xhtml += """
   <url>
     <loc>https://frypatch.github.io/The-Price-of-Remembering/</loc>
