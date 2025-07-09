5e41965cbfdd41011fb7cf193fc5192439cdb71a
frypatch <priceofremembering@altmail.kr>
2025-07-08 21:58:26 -0500

    Add Favicons (#96)

--- publish.py
+++ publish.py
@@ -354,6 +354,10 @@ function display_palette() {
    }
 }
 </script>
+<link rel="apple-touch-icon" sizes="180x180" href="''' + website + '''/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="''' + website + '''/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="''' + website + '''/favicon-16x16.png">
+<link rel="manifest" href="''' + website + '''/site.webmanifest">
 </head>
 <body>
 <script>
